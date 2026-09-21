95a10b42e9d1e3ffe2f83d4f11d02e98cfbc1804
Luis Babo <lcvb124@gmail.com>
2026-09-21 11:59:25 +0100

    aula 1 sib

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
+__author__ = "Luís Babo"
 __credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "Luís Babo"
+__email__ = "pg58817@uminho.pt"
 
 
 
